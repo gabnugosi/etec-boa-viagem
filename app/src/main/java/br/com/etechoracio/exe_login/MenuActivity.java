3b3fbe18238716f7917fd2ce4e7215d9be2dfc03
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-04-09 22:13:39 -0300

    Primeiro commit no boaViagem

---
+++ app/src/main/java/br/com/etechoracio/exe_login/MenuActivity.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.exe_login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
